25388da54471a18b9db684c82d0c8ccd4d187f47
Bhavleen <singhbhavleen3@gmail.com>
2021-04-11 18:29:09 +0530

    days left feature left

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
 const App = () => {
   const currDate = new Date();
-  const currDay = currDate.getDay();
-  const currMonth = currDate.getMonth();
   const daysInWeek = [
     "Sunday",
     "Monday",
@@ -25,6 +26,25 @@ const App = () => {
     "November",
     "December",
   ];
+  const [currDay, setCurrDay] = useState(0);
+  const [daysLeft, setDaysLeft] = useState(0);
+
+  useEffect(() => {
+    setCurrDay(
+      Math.ceil(
+        (currDate -
+          new Date(currDate.getFullYear(), 0, 1)) /
+          86400000
+      )
+    );
+    setDaysLeft(
+      Math.ceil(
+        (new Date(currDate.getFullYear(), 12, 0) -
+          currDate) /
+          86400000
+      )
+    );
+  }, []);
 
   return (
     <div>
@@ -40,7 +60,17 @@ const App = () => {
           ${currDate.getDate()}, 
           ${currDate.getFullYear()} is ...`}
         </h5>
-        <input type="text" placeholder="0" />
+        <div className="container">
+          <p>Day {currDay}</p>
+        </div>
+        <p className="smText">
+          Day of the year is a number between 1 and 365 (in
+          2021), January 1 is day 1.
+        </p>
+        <p className="smText">
+          After today <strong>{daysLeft}</strong> days are
+          remaining in this year.
+        </p>
       </section>
     </div>
   );
